6d9273de155bdcde92517dd1dc6a448466026643
henry <jlf4022163@gmail.com>
2021-04-01 20:39:39 +1100

    checkio mission

--- FindLargestvalueInDict.py
+++ FindLargestvalueInDict.py
@@ -1,5 +1,5 @@
 
-# TODO  (Find the largest key based on the key value) => return key (with max value)
+# TODO  (Find the largest key based on the key value) => return key (with max value) => in dict 
 
 stats = {'a':1000, 'b':3000, 'c': 100}
 max_key = max(stats, key=lambda k: stats[k])
@@ -9,13 +9,13 @@ max_k = max(stats, key=stats.get)
 
 test = {'a':1000, 'b':3000, 'c': 100} 
 k = test.get
-print(k)
+# print(k)
 
 
 #! k = get method => container all the keys 
 
 
-# TODO  (Find the largest value based on the key name) => return a sorted list 
+# TODO  (Find the largest value based on the key name) => return a sorted list  =>  in list
 dic = [
         {"name": "bread", "price": 100},
         {"name": "wine", "price": 138},
@@ -26,3 +26,8 @@ dic = [
 
 newlist = sorted(dic, key=lambda k: k['price'],reverse=True) 
 print(newlist)
+
+
+dic_num = {-20: 20, -5: 5, 10: 10, 15: 15}
+
+print(sorted(dic_num, key = lambda k: dic_num[k]))
--- play.py
+++ play.py
@@ -1,16 +1,10 @@
-def between_markers(text: str, begin: str, end: str) -> str:
-    """
-        returns substring between two given markers
-    """
-    # your code here
-    beginIndex = (text.find(begin)+len(begin)) if text.find(begin) !=-1 else 0
-    
-    endIndex = text.find(end) if text.find(end) else len(text)
-    print(beginIndex,endIndex)
-  
-    if beginIndex < endIndex: 
-        return text[beginIndex:endIndex]
-    return ''
+l1 = [1,3,2,5,6,7,8,1]
+print(l1[:2])
 
 
-print(between_markers("No [b]hi","[b]","[/b]"))
+# print(l1[::2])
+# print(l1[1::2])
+
+# s1 = {1,2,3,4,5}
+
+# print(sorted(s1, reverse=True))
