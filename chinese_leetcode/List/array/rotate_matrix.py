89c3c71b68eab1eaf9bb29c9dd7cbdba9e0f0525
lifeng jiang <henry@lifengs-MacBook-Air.local>
2021-05-05 20:56:17 +1000

    rotate 2D list

--- chinese_leetcode/List/array/rotate_matrix.py
+++ chinese_leetcode/List/array/rotate_matrix.py
@@ -1,4 +1,19 @@
 def rotate(m):
     # i = j = 2
-    # (0,0) = (2,0)/ (0,1) = (1,0), (0,2) = (0,0)
-    
+    # (0,0) = (2,0)/ (0,1) = (1,0),/ (0,2) = (0,0) => (n-j,i)
+
+    n = len(m)
+    new_m = [[None]*n for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            new_m[i][j] = m[n-j-1][i]
+            # pass
+
+    return new_m
+
+
+print(rotate([[1,2,3],[4,5,6],[7,8,9]]))
+
+
+
